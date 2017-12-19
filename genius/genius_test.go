eb1ef551d8caad398163f418a37561e63499b842
Gabyshev Semyen <gabyshev.sa@gmail.com>
2017-12-19 20:45:28 +0700

    update example

--- genius/genius_test.go
+++ genius/genius_test.go
@@ -1,5 +1,10 @@
 package genius_test
 
+import (
+	"fmt"
+	"github.com/gabyshev/genius-api/genius"
+)
+
 func ExampleNewClient() {
 	accessToken := "token"
 	client := genius.NewClient(nil, accessToken)
